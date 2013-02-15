1183f1f49bf4b4d091d65ccae8ee41b7231be15f
Michael Halsband <mick@mick-macbookpro.(none)>
2013-02-16 00:17:05 +0200

    initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hahotem::Application.config.secret_token = 'aec38214a6bfbe7128df2be16f332cdc088e45007bfa53f4edcf73359d67335b95850a875bb1277df7073211d469dd51f96ec39afd184d1b5ec8488eee8f0f46'
